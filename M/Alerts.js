72a422fd5aaefece3d94a17fdd141318dc1f49f6
Imanol Ramírez <20230065@ricaldone.edu.sv>
2024-03-17 22:50:57 -0600

    Alerta corregida / Falta validaciones del select

--- M/Alerts.js
+++ M/Alerts.js
@@ -1,10 +1,35 @@
 /*ALERTA DE RESERVA*/
-function alertReservas(){
-    Swal.fire({
-        title: "Éxito",
-        text: "Se registró satisfactoriamente",
-        icon: "success"
-      });
-}
+var selectedItem = document.getElementById("selector").value;
+var defaultItem = document.getElementById("default").value;
 
+var txtName = document.getElementById("nombre").value;
+var txtMail = document.getElementById("email").value;
+
+  /*LIMPIAR CAMPOS*/ 
+  function Limpiar() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("email").value = "";
+  }
+  
+  function alertReservas(){
+    if(txtName == "" || txtMail == "")
+    {
+      Swal.fire({
+        title: "¡Atención!",
+        text: "Ingrese los datos solicitados para completar el proceso",
+        icon: "warning",
+        iconColor: "danger"
+      });
+    }
+    else {
+      Swal.fire({
+          title: "Éxito",
+          text: "Se registró satisfactoriamente",
+          icon: "success",
+          iconColor: "#ffde59"
+        });
+        Limpiar();
+    }
+
+  };
 
