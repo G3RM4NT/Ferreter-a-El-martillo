0c2d4123b5925f5983edfbbb8ed31cd546f4c9b1
Imanol <20230065@ricaldone.edu.sv>
2024-03-29 00:56:56 -0600

    Continuación de programación de Reservas (40%) - Agregué el archivo 'GenerarCards' / Falta terminar de programar esa parte completa

--- M/Alerts.js
+++ M/Alerts.js
@@ -76,28 +76,9 @@ let product = document.getElementById("form-select");
   function SeleccionarItem(option) {resul = option.value;}
 
   function GuardarReservas(){
-    const cardsReservas = `<div class="carD">
-    <div class="face face1">
-        <div class="content">
-            <i class="fa-regular fa-clock fa-spin" style="font-size: 2em; color: #ffffff;"></i>
-            <h3></h3>
-        </div>
-    </div>
-    <div class="face face2 ">
-        <div class="info-reserva">
-            <ul class="pt-4 text-left">
-                <li><b>Nombre:</b>${txtName}</li>
-                <li><b>Correo:</b>${txtMail}</li>
-                <li><b>Teléfono:</b>${txtPhone}</li>
-                <li><b>Producto:</b>${product}</li>
-            </ul>
-        </div>
-        <button class="btn btn-warning" onclick="ReservaEntregada()">Entregado</button>
-    </div>
-</div>`;
-
-    let newCardReserva ;
-    alert(txtName,txtMail,txtPhone);
+const cardsContainer = document.getElementById('cards');
+const card = GenerarCards();
+cardsContainer.innerHTML = card;
 }
 
 function alertReservas() {
@@ -115,6 +96,7 @@ function alertReservas() {
   }
   else {
     Exito();
+    GuardarReservas();
   }
 };
 
